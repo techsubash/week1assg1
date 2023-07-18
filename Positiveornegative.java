e4935a730aaca993362e478b60737bd80d48fd48
techsubash <139789120+techsubash@users.noreply.github.com>
2023-07-18 17:48:15 +0530

    Add files via upload

---
+++ Positiveornegative.java
@@ -0,0 +1,17 @@
+package positiveornegative;
+
+public class Positiveornegative {
+	
+
+	public static void main(String[] args) {
+
+		int  x=100;
+		if(x>0) {
+			System.out.println("the number is positive");
+		}
+		else {
+			System.out.println("the number is  negative");
+		}
+	}
+
+}
