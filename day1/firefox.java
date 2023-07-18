3b23a9fc563ad4f481f7e554e04d21bc65e6b9fc
techsubash <139789120+techsubash@users.noreply.github.com>
2023-07-18 09:21:25 +0530

    Add files via upload

---
+++ day1/firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="firefox";
+float version=100.2f;
+boolean visible=true;
+int year=1998;
+char logo='m';
+System.out.println("name  "+name);
+System.out.println("version   "+version);
+System.out.println("visible  "+visible);
+
+System.out.println("year  "+year);
+System.out.println("logo  "+logo);
+
+
+
+	}
+
+}
